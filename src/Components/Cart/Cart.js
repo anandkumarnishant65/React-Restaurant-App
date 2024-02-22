825726c742d5e95ca0fb907bc7c41e0e909ad3cf
anandkumarnishant65 <anandkumar65@.com>
2024-02-22 19:30:18 +0530

    cartOpenClose

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart=(props)=>{
         {id:'c1', name:'Samosa', amount:2, price:10},
         ].map((item)=><li>{item.name}</li>)}</ul>
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.0</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,14 +3,14 @@ import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
 
-const Header = () => {
+const Header = (props) => {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>    {/* any name can be used in place of onClick because here onClick is not builtin.*/}
         </header>
         <div className={classes['main-image']}>
-            <img src={mealsImage}></img>
+            <img src={mealsImage} alt="Delicious Food"></img>
         </div>
     </Fragment>
 }
